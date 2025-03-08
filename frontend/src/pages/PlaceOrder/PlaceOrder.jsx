b13768d70694b0138a0f1b51f0eb7dce754a1030
doquocviet264 <doquocviet264@gmail.com>
2025-03-08 13:52:30 +0700

    hoan thanh

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,7 +1,8 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import "./PlaceOrder.css";
 import { StoreContext } from "../../context/StoreContext";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 const PlaceOrder = () => {
   const { getTotalCartAmount, token, food_list, cartItem, url } =
     useContext(StoreContext);
@@ -49,9 +50,17 @@ const PlaceOrder = () => {
     else{
       alert("Error");
     }
-    console.log(response.data);
-
   };
+
+  const navigate =useNavigate();
+  useEffect(()=>{
+    if(!token){
+      navigate("/cart")
+    }
+    else if(getTotalCartAmount()===0){
+      navigate("/cart")
+    }
+  },[token])
 
   return (
     <form onSubmit={placeOrder} className="place-order">
