bf571c654b0b01cdeb516956c2914c871a49b3f0
doquocviet264 <doquocviet264@gmail.com>
2025-03-08 12:10:13 +0700

    tao trang thanh toan

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,30 +1,142 @@
-import { useContext } from 'react';
-import './PlaceOrder.css'
-import { StoreContext } from '../../context/StoreContext';
+import { useContext, useState } from "react";
+import "./PlaceOrder.css";
+import { StoreContext } from "../../context/StoreContext";
+import axios from "axios";
 const PlaceOrder = () => {
-  const {getTotalCartAmount} = useContext(StoreContext);
+  const { getTotalCartAmount, token, food_list, cartItem, url } =
+    useContext(StoreContext);
+
+  const [data, setData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    street: "",
+    city: "",
+    state: "",
+    zipcode: "",
+    country: "",
+    phone: "",
+  });
+
+  const onChangeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setData((data) => ({ ...data, [name]: value }));
+  };
+
+  const placeOrder = async (event) => {
+    event.preventDefault();
+    let orderItems = [];
+    food_list.map((item) => {
+      if (cartItem[item._id] > 0) {
+        let itemInfo = item;
+        itemInfo["quantity"] = cartItem[item._id];
+        orderItems.push(itemInfo);
+      }
+    });
+    let orderData = {
+      address: data,
+      items: orderItems,
+      amount: getTotalCartAmount() + 2,
+    };
+    let response = await axios.post(url + "/api/order/place", orderData, {
+      headers: { token },
+    });
+    if (response.data.success) {
+      const { session_url } = response.data;
+      window.location.replace(session_url);
+    }
+    else{
+      alert("Error");
+    }
+    console.log(response.data);
+
+  };
+
   return (
-    <form className='place-order'>
+    <form onSubmit={placeOrder} className="place-order">
       <div className="place-order-left">
-    <p className="title">Thong tin giao hangf</p>
-    <div className="multi-fields">
-      <input type="text" placeholder='First Name' />
-      <input type="text" placeholder='Last name' /> 
-    </div>
-    <input type="text" placeholder='Email address' />
-    <input type="text" placeholder='Steet' />
-    <div className="multi-fields">
-      <input type="text" placeholder='City' />
-      <input type="text" placeholder='State' /> 
-    </div>
-    <div className="multi-fields">
-      <input type="text" placeholder='Zip code' />
-      <input type="text" placeholder='Country' /> 
-    </div>
-    <input type="text" placeholder='Phone' />
+        <p className="title">Thong tin giao hangf</p>
+        <div className="multi-fields">
+          <input
+            required
+            name="firstName"
+            onChange={onChangeHandler}
+            value={data.firstName}
+            type="text"
+            placeholder="First Name"
+          />
+          <input
+            required
+            name="lastName"
+            onChange={onChangeHandler}
+            value={data.lastName}
+            type="text"
+            placeholder="Last name"
+          />
+        </div>
+        <input
+          required
+          name="email"
+          onChange={onChangeHandler}
+          value={data.email}
+          type="text"
+          placeholder="Email address"
+        />
+        <input
+          required
+          name="street"
+          onChange={onChangeHandler}
+          value={data.street}
+          type="text"
+          placeholder="Steet"
+        />
+        <div className="multi-fields">
+          <input
+            required
+            name="city"
+            onChange={onChangeHandler}
+            value={data.city}
+            type="text"
+            placeholder="City"
+          />
+          <input
+            required
+            name="state"
+            onChange={onChangeHandler}
+            value={data.state}
+            type="text"
+            placeholder="State"
+          />
+        </div>
+        <div className="multi-fields">
+          <input
+            required
+            name="zipcode"
+            onChange={onChangeHandler}
+            value={data.zipcode}
+            type="text"
+            placeholder="Zip code"
+          />
+          <input
+            required
+            name="country"
+            onChange={onChangeHandler}
+            value={data.country}
+            type="text"
+            placeholder="Country"
+          />
+        </div>
+        <input
+          name="phone"
+          onChange={onChangeHandler}
+          value={data.phone}
+          type="text"
+          placeholder="Phone"
+        />
       </div>
       <div className="place-order-right">
-      <div className="cart-total">
+        <div className="cart-total">
           <h2>Cart Totals</h2>
           <div>
             <div className="cart-total-details">
@@ -34,19 +146,21 @@ const PlaceOrder = () => {
             <hr />
             <div className="cart-total-details">
               <p>Delvicery Fee</p>
-              <p>${getTotalCartAmount()===0?0:2}</p>
+              <p>${getTotalCartAmount() === 0 ? 0 : 2}</p>
             </div>
             <hr />
             <div className="cart-total-details">
               <b>Total</b>
-              <b>${getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
+              <b>
+                ${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}
+              </b>
             </div>
           </div>
-          <button> thanh toan</button>
+          <button type="submit"> thanh toan</button>
         </div>
       </div>
     </form>
-  )
-}
+  );
+};
 
-export default PlaceOrder
+export default PlaceOrder;
